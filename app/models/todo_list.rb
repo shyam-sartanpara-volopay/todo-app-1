2e7f1aa6405a6e396f07e70a6cb2f3fc820ed69b
Sivani-volopay <sivani@volopay.co>
2025-03-17 16:54:25 +0530

    added status for todo_lists(todo_list/:todolist_id/todos/id)

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -2,4 +2,5 @@ class TodoList < ApplicationRecord
   belongs_to :user
   has_many :todos, dependent: :destroy
   validates :name, presence: true, uniqueness: { case_sensitive: false, message: 'has already been added' }
+  enum status: { active: 'active', pending: 'pending', completed: 'completed' }
 end
