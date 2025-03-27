bf3d8f7ef345ff34332f423391957e05cce59285
Prashant Sah <prashant.sah@volopay.co>
2025-03-27 15:06:33 +0530

    removed test log

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,6 @@ class Task < ApplicationRecord
     belongs_to :todo_list
 
     has_many :collaborations, through: :todo_list
-
     validates :title, presence: true
     validates :completed, inclusion: { in: [true, false] }
 end
